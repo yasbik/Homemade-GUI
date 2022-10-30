5adc2725e9ca643e8647b5127865c4777b947516
Abu Yasin Sabik <yasin00sabik@gmail.com>
2022-10-30 04:42:29 -0400

    Created Button class

---
+++ src/Button.java
@@ -0,0 +1,40 @@
+public class Button extends GUIelement {
+    
+    public Button ( double xCenterIn, double yCenterIn,
+                    double halfWidthIn, double halfHeightIn, String title) {
+
+                        super(xCenterIn, yCenterIn, halfWidthIn, halfHeightIn, title, false);
+    }
+    
+    public void draw() {
+        if (highlighted) {
+            // draw light grey rectangle
+            StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+            StdDraw.filledRectangle(xCentre, yCentre, halfWidth, halfHeight);
+        }
+        else {
+            // draw white rectancle
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledRectangle(xCentre, yCentre, halfWidth, halfHeight);
+        }
+
+        // draw outline
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius();
+        StdDraw.rectangle(xCentre, yCentre, halfWidth, halfHeight);
+    }
+
+    public boolean handleClick(double x, double y) {
+        if (!super.handleClick(x, y)) {
+            return false;
+        }
+        // inside the buttton
+        else {
+            // change highlight to true and reset the buttons
+            highlighted = true;
+            Utilities.waitMouseUp();
+            draw();
+            return true;
+        }
+    }
+}
